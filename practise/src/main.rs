f2232c9ae4671b36fae4e36569450ec7bfec8da1
wilkodev9 <wilkodeveloper@gmail.com>
2023-04-09 04:18:50 +0200

    project soft reset

--- practise/src/main.rs
+++ practise/src/main.rs
@@ -1,11 +1,21 @@
 /*
 
-test and files to add:
+features:
    word length checker
    percentage of sentence thats words and digits and amount
    upper and lowercase case charcter amount + percentage
    word counter
    charcter counter
+   sort longest word to shortest word
+   checks if the numbers are even or odd
+   checks sentence lenght
+   create a text document to save this data to
+   
+to-do:
+   sepperate input out of main
+   put the unit tests into the files of original
+   write out the the pseudo code for all the things that need to get done
+   create a flow chart for all teh interactions
 */
 
 use crate::odd_or_even::odd_or_even::odd_or_even;
@@ -16,5 +26,12 @@ pub mod odd_or_even;
 pub mod sentence_lenght;
 
 fn main() {
-   odd_or_even()
+   x = 
+}
+
+fn input() -> String {
+   let mut numb = String::new();
+   std::io::stdin().read_line(&mut numb);
+   // let filter = number_filter(numb);
+   return numb;
 }
