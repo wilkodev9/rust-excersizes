06a1496b3acc2134f131ad2d14c337154c97262e
wilkodev9 <wilkodeveloper@gmail.com>
2023-04-10 22:54:50 +0200

    added todo list update

--- practise/src/main.rs
+++ practise/src/main.rs
@@ -2,20 +2,18 @@
 
 features:
    word length checker
-   percentage of sentence thats words and digits and amount
+   percentage of sentence thats charters and digits and special charcters and amount
    upper and lowercase case charcter amount + percentage
    word counter
    charcter counter
    sort longest word to shortest word
    checks if the numbers are even or odd
    checks sentence lenght
+   counts special charcters
    create a text document to save this data to
    
 to-do:
-   sepperate input out of main
-   put the unit tests into the files of original
-   write out the the pseudo code for all the things that need to get done
-   create a flow chart for all teh interactions
+   make psuedo code 
 */
 
 fn main() {
